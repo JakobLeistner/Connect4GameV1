a3955e0100ba7773c0d4faa31a2e030a2bdd520a
susha77 <alexandra.kaiser@infoteam.de>
2023-03-24 12:10:20 +0100

    Added RTPHub and EventHandler, addded code to Startup

---
+++ Connect4Game/Connect4Game/Connect4Game_RestAPI/EventHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Connect4Game_RestAPI
+{
+    public class EventHandler
+    {
+        public EventHandler(ConnectionService connectionService)
+        {
+            //parameter connectionService nur hinzugefügt, weil startup.cs das scheinbar möchte
+        }
+        public void EventHandlerInitalize(string HubUrlOderSo)
+        {
+
+        }
+    }
+}
